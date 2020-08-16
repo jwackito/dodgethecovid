82a0eb7099e9ab2c8d53b7981f4da6ce49ca3e66
Lautaro Nahuel De León <laudleon@gmail.com>
2020-08-16 19:32:00 -0300

    Code Reorganization

---
+++ Dodge The Creeps/src/Player.gd
@@ -0,0 +1,54 @@
+extends Area2D
+
+signal hit
+var screen_size
+
+export var speed = 400
+export var life  = 100
+export var mask  = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func get_velocity_from_input():
+	var velocity = Vector2()
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	
+	return velocity.normalized() * speed
+
+func _process(delta):
+	var velocity = get_velocity_from_input()
+	if velocity.length() > 0:	
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	if velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.flip_v = velocity.y > 0
+	
+func _on_Player_body_entered(_body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
