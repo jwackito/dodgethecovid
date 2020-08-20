e56e78a1e0bf0849bad2a62f1d533252b86a8d09
Joaquín Bogado <jbogado@linti.unlp.edu.ar>
2020-08-20 17:15:42 -0300

    Added GUI scenes

--- Dodge The Creeps/src/Hud.gd
+++ Dodge The Creeps/src/Hud.gd
@@ -14,7 +14,7 @@ func show_message(text):
 func show_game_over():
 	show_message("Game Over")
 	yield($MessageTimer, "timeout")
-	$Message.text = "Dodge the Creeps"
+	$Message.text = "Escape from Covid-19"
 	$Message.show()
 	yield(get_tree().create_timer(1), "timeout")
 	$StartButton.show()
