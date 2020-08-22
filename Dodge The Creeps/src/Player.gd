ed3c750869fcb73d6def9980fde66f86b602f344
Lautaro Nahuel De León <laudleon@gmail.com>
2020-08-22 20:23:52 -0300

    Added scroll

--- Dodge The Creeps/src/Player.gd
+++ Dodge The Creeps/src/Player.gd
@@ -2,17 +2,19 @@ class_name Player
 extends Area2D
 
 signal hit(covid, mask)
+
 var screen_size
+var level_size
 
 export var speed = 400
 export var covid  = 0
 export var mask  = 0
 
-const Person = preload("Person.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	level_size = Vector2(3000, 3000)
 
 func get_velocity_from_input():
 	var velocity = Vector2()
@@ -30,13 +32,13 @@ func get_velocity_from_input():
 
 func _process(delta):
 	var velocity = get_velocity_from_input()
-	if velocity.length() > 0:	
+	if velocity.length() > 0:
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	position.x = clamp(position.x, 0, level_size.x)
+	position.y = clamp(position.y, 0, level_size.y)
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.flip_v = false
