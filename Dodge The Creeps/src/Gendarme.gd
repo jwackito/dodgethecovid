7950c3885e512347c65abb4d17070dda3b28c3be
Joaquin Gamba <joaquingamba.13@gmail.com>
2020-08-28 21:26:49 -0300

    Add Police object and add angry gesticure to a Gendarme

--- Dodge The Creeps/src/Gendarme.gd
+++ Dodge The Creeps/src/Gendarme.gd
@@ -1,9 +1,9 @@
 class_name Gendarme
 extends Person
 
-
 export var vision_range: int = 1000
 var _enemy_node: Node2D = null
+var _is_in_pursuit: bool = false
 
 
 func _init() -> void:
@@ -11,6 +11,7 @@ func _init() -> void:
 
 
 func _ready() -> void: 
+	_set_neutral_gesticure()
 	_set_sprite_and_collision($AnimatedSprite, $Collision)
 
 
@@ -24,5 +25,27 @@ func _process(delta: float):
 	if (_enemy_node and _enemy_node.is_inside_tree()):
 		var distance: Vector2 = _enemy_node.position - self.position
 		if (distance.length() < vision_range):
+			if !_is_in_pursuit:
+				_set_in_pursuit()
 			velocity = distance.normalized() * speed
+		elif _is_in_pursuit:
+			_unset_in_pursuit()
 	._move(delta)
+
+
+func _set_in_pursuit():
+	_is_in_pursuit = true
+	_set_angry_gesticure()
+
+func _unset_in_pursuit():
+	_is_in_pursuit = false
+	_set_neutral_gesticure()
+
+
+func _set_neutral_gesticure():
+	$AnimatedGesticure.animation = "none"
+	$AnimatedGesticure.stop()
+
+func _set_angry_gesticure():
+	$AnimatedGesticure.animation = "angry"
+	$AnimatedGesticure.play()
---
+++ Dodge The Creeps/src/Police.gd
@@ -0,0 +1,24 @@
+class_name Police
+extends Person
+
+
+export var vision_range: int = 1000
+var _enemy_node: Node2D = null
+
+
+func _init() -> void:
+	default_sprite_name = "default"
+
+
+func _ready() -> void: 
+	_set_sprite_and_collision($AnimatedSprite, $Collision)
+
+
+func init(new_position: Vector2, direction, _enemy: Node2D) -> Police:
+	self._enemy_node = _enemy
+	_init_person(new_position, direction)
+	return self
+
+
+func _process(delta: float):
+	._person_process(delta)
