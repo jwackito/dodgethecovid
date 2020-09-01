0e6f0d45cb3906900ffdbf66e3ca3963f323738b
Lautaro Nahuel De León <laudleon@gmail.com>
2020-08-31 21:49:22 -0300

    Added random classes to manage items and enemies spawn and positions

--- Dodge The Creeps/src/LevelOne.gd
+++ Dodge The Creeps/src/LevelOne.gd
@@ -1,22 +1,20 @@
 class_name LevelOne
 extends Node
 
-export (PackedScene) var PersonWithMaskPackage
-export (PackedScene) var PersonWithoutMaskPackage
-export (PackedScene) var PersonVisiblySickPackage
-export (PackedScene) var GendarmePackage
-export (PackedScene) var PolicePackage
-#onready var SpawnEnemyTimer = get_node("SpawnEnemyTimer")
-
 export (PackedScene) var MaskPackage
 export (PackedScene) var CDSPackage
 export (PackedScene) var HCQSPackage
 export (PackedScene) var GelAlcoholPackage
 export (PackedScene) var PermitPackage
+
 export (PackedScene) var StartLevel
 export (PackedScene) var EndLevel
 
-#onready var SpawnItemTimer = get_node("SpawnItemTimer")
+export (PackedScene) var PersonWithMaskPackage
+export (PackedScene) var PersonWithoutMaskPackage
+export (PackedScene) var PersonVisiblySickPackage
+export (PackedScene) var GendarmePackage
+export (PackedScene) var PolicePackage
 
 export var level_size = Vector2(5000,3500)
 export var start_level_position = Vector2(0, 0)
@@ -24,25 +22,18 @@ export var end_level_position = Vector2(5000, 3500)
 
 signal level_ended(covid, mask, permits)
 
-const enemies = {
-	"person_with_mask": {
-		"spawn_percent": 40,
-	},
-	"person_without_mask": {
-		"spawn_percent": 60,
-	},
-	"person_visibly_sick": {
-		"spawn_percent": 80,
-	},
-	"gendarme": {
-		"spawn_percent": 100,
-	},
-	"police": {
-		"spawn_percent": 0,
-	},
-}
+var RandomGen = preload("utils/random_points_surface.gd")
+var Spawner   = preload("utils/spawner.gd")
+
+var items;
+var enemies;
+
+var items_positions = RandomPointsSurface.new(level_size.x, level_size.y, 500, 500)
+var items_spawner   = Spawner.new("items")
+var enemies_spawner = Spawner.new("enemies")
 
 func _ready():
+	items = { "mask": MaskPackage, "gel_alcohol": GelAlcoholPackage, "hcqs": HCQSPackage, "cds": CDSPackage, "permit": PermitPackage }
 	randomize()
 	$LevelLayer/SpawnEnemyTimer.start()
 	$LevelLayer/SpawnItemTimer.start()
@@ -56,7 +47,6 @@ func _create_militia(Militia):
 	var gendarme = Militia.instance().init(gendarme_position, direction, $LevelLayer/Player)
 	add_child(gendarme)
 
-
 func _create_civil(Civil):
 	$LevelLayer/InitPersonPath/InitPosition.set_offset(randi()) # get random position
 	var direction = $LevelLayer/InitPersonPath/InitPosition.rotation + PI/2
@@ -66,22 +56,23 @@ func _create_civil(Civil):
 	add_child(person)
 
 func _on_SpawnEnemyTimer_timeout():
-	var random_number = rand_range(0, 100)
-	if random_number < enemies.person_with_mask.spawn_percent:
-		_create_civil(PersonWithMaskPackage)
-	elif random_number < enemies.person_without_mask.spawn_percent:
-		_create_civil(PersonWithoutMaskPackage)
-	elif random_number < enemies.person_visibly_sick.spawn_percent:
-		_create_civil(PersonVisiblySickPackage)
-	elif random_number < enemies.police.spawn_percent:
-		_create_militia(PolicePackage)
-	elif random_number < enemies.gendarme.spawn_percent:
-		_create_militia(GendarmePackage)
+	match enemies_spawner.take():
+		"person_with_mask":
+		  _create_civil(PersonWithMaskPackage)
+		"person_without_mask":
+		  _create_civil(PersonWithoutMaskPackage)
+		"person_visibly_sick":
+		  _create_civil(PersonVisiblySickPackage)
+		#"police":
+		#  _create_militia(PolicePackage)
+		"gendarme":
+		  _create_militia(GendarmePackage)
 
 func _spaw_item(Item):
 	var item = Item.instance()
-	item.position.x = randi()%int(level_size.x)
-	item.position.y = randi()%int(level_size.y)
+	var new_item_position = items_positions.take()
+	item.position.x = new_item_position[0]
+	item.position.y = new_item_position[1]
 	add_child(item)
 
 func _spawn_start_end_level():
@@ -94,9 +85,7 @@ func _spawn_start_end_level():
 	$LevelLayer/Player.position = start_level_position
 
 func _on_SpawnItemTimer_timeout():
-	var items = [MaskPackage, GelAlcoholPackage, HCQSPackage, CDSPackage, PermitPackage]
-	_spaw_item(items[randi()%5])
-
+	_spaw_item(items[items_spawner.take()])
 
 func _on_Player_end_level(covid, mask, permits):
 	emit_signal("level_ended", covid, mask, permits)
