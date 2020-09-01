3a116f42bdc9ff99be5e1d2b33e0bd505d9185ef
Joaquín Bogado <jbogado@linti.unlp.edu.ar>
2020-09-01 16:49:54 -0300

    Solved the difficulty levels issue

--- Dodge The Creeps/src/MainGame.gd
+++ Dodge The Creeps/src/MainGame.gd
@@ -7,8 +7,8 @@ extends Control
 
 # ######################
 # Difficulty:
-#   * "Im not in a risk group": covid resets, permits at least 1, mask at least 50%, 1 level retry
-#   * "Resfriadinho":  covid resets, 1 level retry except first
+#   * "Im not in a risk group": covid resets, permits at least 1, mask at least 50%, 1 level retry per level
+#   * "Resfriadinho":  covid resets, 1 level
 #   * "Im legend": whatever you got in a level, is what you get in the next, no retrys
 # ######################
 var difficulty
@@ -23,7 +23,7 @@ func _ready():
 	usercovid = 0
 	usermask = 0
 	userpermits = 0
-	level_retrys = 0
+	level_retrys = 1
 	$VBoxContainer/DifficultyContainer/Difficulty.add_item("I'm not in a risk group")
 	$VBoxContainer/DifficultyContainer/Difficulty.add_item("Resfriadinho")
 	$VBoxContainer/DifficultyContainer/Difficulty.add_item("I'm Legend")
@@ -33,36 +33,19 @@ func _on_Player_level_ended(covid, mask, permits):
 	usercovid = covid
 	usermask = mask
 	userpermits = permits
-
-func _on_Level_level_ended(covid, mask, permits):
-	remove_child(level)
-	$VBoxContainer.show()
-	$VBoxContainer/Start.text = "Next Level"
 	if difficulty == 0:
 		usermask = max(50, mask)
 		userpermits = max(1, permits)
+		level_retrys = 1
 	if difficulty <= 1:
 		usercovid = 0
-		level_retrys = 1
+		
+func _on_Level_level_ended(_covid, _mask, _permits):
+	remove_child(level)
+	$VBoxContainer.show()
+	$VBoxContainer/Title.text = "Get Ready!"
+	$VBoxContainer/Start.text = "Next Level"
 	$VBoxContainer/DifficultyContainer.hide()
-
-func retryLevel():
-	difficulty = $VBoxContainer/DifficultyContainer/Difficulty.get_selected_id()
-	if difficulty == 0:
-		usermask = max(50, usermask)
-		userpermits = max(1, userpermits)
-	level = load("scenes/LevelOne.tscn").instance()
-	add_child(level)
-	level.connect("level_ended", self, "_on_Level_level_ended")
-	var player = level.get_node("LevelLayer/Player")
-	player.covid = usercovid
-	player.mask = usermask
-	player.permits = userpermits
-	#player.connect("end_level", self, "_on_Player_level_ended")
-	player.connect("gameover", self, "_on_Player_gameovered")
-	player.emit_signal("hit", usercovid, usermask)
-	player.emit_signal("permit_update", userpermits)
-	$VBoxContainer.hide()
 
 func _on_Player_gameovered():
 	remove_child(level)
@@ -73,14 +56,13 @@ func _on_Player_gameovered():
 		$VBoxContainer/DifficultyContainer.show()
 	else:
 		level_retrys -= 1
-		retryLevel()
+		_on_Start_pressed()
 
 func _on_Start_pressed():
 	difficulty = $VBoxContainer/DifficultyContainer/Difficulty.get_selected_id()
 	if difficulty == 0:
 		usermask = max(50, usermask)
 		userpermits = max(1, userpermits)
-		level_retrys = 1
 	level = load("scenes/LevelOne.tscn").instance()
 	add_child(level)
 	level.connect("level_ended", self, "_on_Level_level_ended")
@@ -88,8 +70,19 @@ func _on_Start_pressed():
 	player.covid = usercovid
 	player.mask = usermask
 	player.permits = userpermits
-	#player.connect("end_level", self, "_on_Player_level_ended")
+	player.connect("end_level", self, "_on_Player_level_ended")
 	player.connect("gameover", self, "_on_Player_gameovered")
 	player.emit_signal("hit", usercovid, usermask)
 	player.emit_signal("permit_update", userpermits)
 	$VBoxContainer.hide()
+
+func _on_Difficulty_item_selected(index):
+	usercovid = 0
+	usermask = 0
+	userpermits = 0
+	if index == 0:
+		level_retrys = 3
+	if index == 1:
+		 level_retrys = 1
+	if index == 2:
+		level_retrys = 0
